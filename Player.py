80797f2ab305fbe14f1722cf04c1e503a4e7b304
Stover C Mardis <stover.mardis@uvm.edu>
2019-10-24 12:37:17 -0400

    style changes

--- Player.py
+++ Player.py
@@ -2,13 +2,13 @@ import pygame
 
 
 class Player:
-    def __init__(self, money=1200, is_player=False):
+    def __init__(self, money=1200, isPlayer=False):
         self.money = money
-        self.is_player = is_player
-        self.has_jail_card = False
-        self.owned_properties = []
+        self.isPlayer = isPlayer
+        self.hasJailCard = False
+        self.ownedProperties = []
 
-    def take_money(self, amount):
+    def takeMoney(self, amount):
         if self.money >= amount:
             self.money -= amount
             return 0
@@ -16,11 +16,11 @@ class Player:
             self.money = 0
             return -1 * (self.money - amount)
 
-    def give_money(self, amount):
+    def giveMoney(self, amount):
         self.money += amount
 
-    def has_property(self, prop):
-        for p in self.owned_properties:
+    def hasProperty(self, prop):
+        for p in self.ownedProperties:
             if p == prop:
                 return True
         return False
