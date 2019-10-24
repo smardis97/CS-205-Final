aa241c172f226ae39a015a499608af836186789f
mkyle98 <kylem.sde@gmail.com>
2019-10-24 12:17:47 -0400

    Added Properties values

--- tile.py
+++ tile.py
@@ -1,18 +1,46 @@
 class Tile:
-    def __init__(self):
     def onLand(self):
+        raise NotImplementedError
+
+
 
 class Properties(Tile):
-    value = 0.0
-    group = ""
+    def __init__(self, value, group, name, owner):
+        self.name = name
+        self.value = value
+        self.group = group
+        self.owner = owner
+    
+    def setOwner(self, owner):
+        self.owner = owner
+    def setValue(self, value):
+        self.value = value
+    def setGroup(self, group):
+        self.group = group
+    def setName(self, name):
+        self.name = name
+    def getOwner(self):
+        return self.owner
+    def getValue(self):
+        return self.value
+    def getGroup(self):
+        return self.group
+    def getName(self):
+        return self.name
 
-    def onLand(self):
+    
 
-class Go(Tile):
-    def onLand(self):
+    #def onLand(self):
 
-class Parking(Tile):
-    def onLand(self):
 
-class goToJail(Tile):
-    def onLand(self):
+# class Go(Tile):
+#     #def onLand(self):
+
+# class Parking(Tile):
+#     #def onLand(self):
+
+# class goToJail(Tile):
+#     #def onLand(self):
+
+# class cardTile(Tile):
+#     #def onLand(self):
