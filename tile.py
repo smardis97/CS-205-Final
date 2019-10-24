d6d2257ae57793e0ec0551dc9805e2bd1231e4cb
mkyle98 <kylem.sde@gmail.com>
2019-10-24 11:54:55 -0400

    Update tile.py

---
+++ tile.py
@@ -0,0 +1,18 @@
+class Tile:
+    def __init__(self):
+    def onLand(self):
+
+class Properties(Tile):
+    value = 0.0
+    group = ""
+
+    def onLand(self):
+
+class Go(Tile):
+    def onLand(self):
+
+class Parking(Tile):
+    def onLand(self):
+
+class goToJail(Tile):
+    def onLand(self):
