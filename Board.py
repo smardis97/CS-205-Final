a0449f24d1122fe5cf25c1a4155ef6c53a203bd1
Stover C Mardis <stover.mardis@uvm.edu>
2019-11-02 20:41:33 -0400

    filled constructBoard

--- Board.py
+++ Board.py
@@ -11,7 +11,49 @@ class Board:
         self.constructBoard()
 
     def constructBoard(self):
-        pass
+        self.addTile(Tile.Go())
+        self.addTile(Tile.Property(60, "Mediterranean Avenue", (2, 10, 30, 90, 160), 50, "Brown"))
+        self.addTile(Tile.CardTile())
+        self.addTile(Tile.Property(60, "Baltic Avenue", (4, 20, 60, 180, 320, 450), 50, "Brown"))
+        self.addTile(Tile.Tax())
+        self.addTile(Tile.Property(200, "Reading Railroad", (25, 50, 100, 200), "Railroad"))
+        self.addTile(Tile.Property(100, "Oriental Avenue", (6, 30, 90, 270, 400, 550), 50, "Light Blue"))
+        self.addTile(Tile.CardTile())
+        self.addTile(Tile.Property(100, "Vermont Avenue", (6, 30, 90, 270, 400, 550), 50, "Light Blue"))
+        self.addTile(Tile.Property(120, "Connecticut Avenue", (8, 40, 100, 300, 450, 600), 50, "Light Blue"))
+
+        self.addTile(Tile.Jail)
+        self.addTile(Tile.Property(140, "St. Charles Place", (10, 50, 150, 450, 625, 750), 100, "Pink"))
+        self.addTile(Tile.Property(150, "Electric Company", (4, 10), "Utility"))
+        self.addTile(Tile.Property(140, "States Avenue", (10, 50, 150, 450, 625), 100, "Pink"))
+        self.addTile(Tile.Property(160, "Virginia Avenue", (12, 60, 180, 500, 700), 100, "Pink"))
+        self.addTile(Tile.Property(200, "Pennsylvania Railroad", (25, 50, 100, 200), "Railroad"))
+        self.addTile(Tile.Property(180, "St. James Place", (14, 70, 200, 550, 750, 950), 100, "Orange"))
+        self.addTile(Tile.CardTile())
+        self.addTile(Tile.Property(180, "Tennessee Avenue", (14, 70, 200, 550, 750, 950), 100, "Orange"))
+        self.addTile(Tile.Property(200, "New York Avenue", (16, 80, 220, 600, 800, 1000), 100, "Orange"))
+
+        self.addTile(Tile.FreeParking())
+        self.addTile(Tile.Property(220, "Kentucky Avenue", (18, 90, 250, 700, 875), 150, "Red"))
+        self.addTile(Tile.CardTile())
+        self.addTile(Tile.Property(220, "Indiana Avenue", (18, 90, 250, 700, 875), 150, "Red"))
+        self.addTile(Tile.Property(240, "Illinois Avenue", (20, 100, 300, 750, 925, 1100), 150, "Red"))
+        self.addTile(Tile.Property(200, "B. & O. Railroad", (25, 50, 100, 200), "Railroad"))
+        self.addTile(Tile.Property(260, "Atlantic Avenue", (22, 110, 330, 800, 975, 1150), 150, "Yellow"))
+        self.addTile(Tile.Property(260, "Ventnor Avenue", (22, 110, 330, 800, 975, 1150), 150, "Yellow"))
+        self.addTile(Tile.Property(150, "Water Works", (4, 10), "Utility"))
+        self.addTile(Tile.Property(280, "Marvin Gardens", (24, 120, 360, 850, 1025, 1200), 150, "Yellow"))
+
+        self.addTile(Tile.GoToJail())
+        self.addTile(Tile.Property(300, "Pacific Avenue", (26, 130, 390, 900, 1100, 1275), 200, "Green"))
+        self.addTile(Tile.Property(300, "North Carolina Avenue", (26, 130, 390, 900, 1100, 1275), 200, "Green"))
+        self.addTile(Tile.CardTile())
+        self.addTile(Tile.Property(320, "Pennsylvania Avenue", (28, 150, 450, 1000, 1200, 1400), 200, "Green"))
+        self.addTile(Tile.Property(200, "Short Line", (25, 50, 100, 200), "Railroad"))
+        self.addTile(Tile.CardTile())
+        self.addTile(Tile.Property(350, "Park Place", (35, 175, 500, 1100, 1300, 1500), 200, "Blue"))
+        self.addTile(Tile.Tax())
+        self.addTile(Tile.Property(400, "Boardwalk", (50, 200, 600, 1400, 1700, 2000), 200, "Blue"))
 
     def addPlayer(self, player):
         if not self.gameStarted:
