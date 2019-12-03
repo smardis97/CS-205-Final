178ae20719205d22815fe46171e88c6fb772318e
Stover C Mardis <stover.mardis@uvm.edu>
2019-12-03 17:36:03 -0500

    refactoring buttons

--- buttons.py
+++ buttons.py
@@ -47,54 +47,54 @@ class BUTTONS:
                         sumDice -= 1
                         print(sumDice)
                     # blue one near corner
-                    if self.circleX == 1065+self.distance - 630 and self.circleY == 771+self.distance and sumDice != 0:
+                    if self.circleX == 1065 + self.distance - 630 and self.circleY == 771+self.distance and sumDice != 0:
                         self.circleX -= 2 * self.distance
                         sumDice-=1
                         print(sumDice)
-                    if self.circleX == 1065 + self.distance - 630 - 2 * self.distance and self.circleY != 771+self.distance-630 and sumDice!=0:
-                        self.circleY-=70
-                        sumDice-=1
+                    if self.circleX == 1065 + self.distance - 630 - 2 * self.distance and self.circleY != 771 + self.distance - 630 and sumDice != 0:
+                        self.circleY -= 70
+                        sumDice -= 1
                         print(sumDice)
                     # light blue near corner
-                    if self.circleX == 1065+self.distance-630-2*(self.distance) and self.circleY == 771+self.distance-630 and sumDice!=0:
-                        self.circleY-=2*(self.distance)
-                        sumDice-=1
+                    if self.circleX == 1065 + self.distance - 630 - 2 * self.distance and self.circleY == 771 + self.distance - 630 and sumDice != 0:
+                        self.circleY -= 2 * self.distance
+                        sumDice -= 1
                         print(sumDice)
-                    if self.circleX != 1065+self.distance-2*(self.distance) and self.circleY == 771+self.distance-630-2*(self.distance) and sumDice!=0:
-                        self.circleX+=70
-                        sumDice-=1
+                    if self.circleX != 1065 + self.distance - 2 * self.distance and self.circleY == 771+self.distance - 630 - 2 * self.distance and sumDice != 0:
+                        self.circleX += 70
+                        sumDice -= 1
                         print(sumDice)
                     # orange near corner
-                    if self.circleX == 1065+self.distance-2*(self.distance) and self.circleY == 771+self.distance-630-2*(self.distance) and sumDice!=0:
-                        self.circleX+=2*(self.distance)
-                        sumDice-=1
+                    if self.circleX == 1065 + self.distance - 2 * self.distance and self.circleY == 771 + self.distance - 630 - 2 * self.distance and sumDice != 0:
+                        self.circleX += 2 * self.distance
+                        sumDice -= 1
                         print(sumDice)
-                    if self.circleX == 1065+self.distance and self.circleY != 771+self.distance-2*(self.distance) and sumDice!=0:
-                        self.circleY+=70
-                        sumDice-=1
+                    if self.circleX == 1065 + self.distance and self.circleY != 771 + self.distance - 2 * self.distance and sumDice != 0:
+                        self.circleY += 70
+                        sumDice -= 1
                         print(sumDice)
                     # yellow near corner
-                    if self.circleX == 1065+self.distance and self.circleY == 771+self.distance-2*self.distance and sumDice!=0:
-                        self.circleY+=2*(self.distance)
-                        sumDice-=1
+                    if self.circleX == 1065 + self.distance and self.circleY == 771 + self.distance - 2 * self.distance and sumDice != 0:
+                        self.circleY += 2 * self.distance
+                        sumDice -= 1
                         print(sumDice)
                 self.dice1 = random.randint(1, 6)
                 self.dice2 = random.randint(1, 6)
         else:
-            pygame.draw.rect(self.screen, blue, (self.width/2-50, 275, 100, 50))
+            pygame.draw.rect(self.screen, blue, (self.width / 2 - 50, 275, 100, 50))
         # reset
-        if self.width/2+50 > mouse[0] > self.width/2-50 and 400 > mouse[1] > 350:
-            pygame.draw.rect(self.screen, lightGreen, (self.width/2-50, 350, 100, 50))
-            if click[0]==1:
+        if self.width / 2 + 50 > mouse[0] > self.width / 2 - 50 and 400 > mouse[1] > 350:
+            pygame.draw.rect(self.screen, lightGreen, (self.width / 2 - 50, 350, 100, 50))
+            if click[0] == 1:
                 self.circleX = 295 + 910 - 140 - 70 + 1 + 69 + self.distance
                 self.circleY = 910 - 140 + 1 + self.distance
         else:
-            pygame.draw.rect(self.screen, green, (self.width/2-50, 350, 100, 50))
+            pygame.draw.rect(self.screen, green, (self.width / 2 - 50, 350, 100, 50))
         # quit
-        if self.width/2+50 > mouse[0] > self.width/2-50 and 475 > mouse[1] > 425:
-            pygame.draw.rect(self.screen, lightBlue, (self.width/2-50, 425, 100, 50))
+        if self.width / 2 + 50 > mouse[0] > self.width / 2 - 50 and 475 > mouse[1] > 425:
+            pygame.draw.rect(self.screen, lightBlue, (self.width / 2 - 50, 425, 100, 50))
         else:
-            pygame.draw.rect(self.screen, blue, (self.width/2-50, 425, 100, 50))
+            pygame.draw.rect(self.screen, blue, (self.width / 2 - 50, 425, 100, 50))
 
         startSurf, startRect = self.textObjects("Start",self.smallFont)
         diceSurf, diceRect = self.textObjects("Dice",self.smallFont)
@@ -102,10 +102,10 @@ class BUTTONS:
         quitSurf, quitRect = self.textObjects("Quit",self.smallFont)
 
 
-        startRect.center=(self.width/2, 200+25)
-        diceRect.center=(self.width/2, 275+25)
-        resetRect.center=(self.width/2, 350+25)
-        quitRect.center=(self.width/2, 425+25)
+        startRect.center=(self.width / 2, 200 + 25)
+        diceRect.center=(self.width / 2, 275 + 25)
+        resetRect.center=(self.width / 2, 350 + 25)
+        quitRect.center=(self.width / 2, 425 + 25)
 
         self.screen.blit(startSurf, startRect)
         self.screen.blit(diceSurf, diceRect)
@@ -116,15 +116,15 @@ class BUTTONS:
         mouse = pygame.mouse.get_pos()
         click = pygame.mouse.get_pressed()
         # start
-        if self.width/2+100 > mouse[0] > self.width/2-100 and 570 > mouse[1] > 470:
-            pygame.draw.rect(self.screen, lightGreen, (self.width/2-100, 470, 200, 100))
+        if self.width / 2 + 100 > mouse[0] > self.width / 2 - 100 and 570 > mouse[1] > 470:
+            pygame.draw.rect(self.screen, lightGreen, (self.width / 2 - 100, 470, 200, 100))
             if click[0] == 1:
                 self.programState = 1
         else:
-            pygame.draw.rect(self.screen, green, (self.width/2-100, 470, 200, 100))
+            pygame.draw.rect(self.screen, green, (self.width / 2 - 100, 470, 200, 100))
         # music
-        if self.width/2-200 > mouse[0] > self.width/2-400 and 570 > mouse[1] > 470:
-            pygame.draw.rect(self.screen, lightBlue, (self.width/2-400, 470, 200, 100))
+        if self.width / 2 - 200 > mouse[0] > self.width / 2 - 400 and 570 > mouse[1] > 470:
+            pygame.draw.rect(self.screen, lightBlue, (self.width / 2 - 400, 470, 200, 100))
             if click[0] == 1 and self.musicState:
                 pygame.mixer.music.pause()
                 self.musicState = False
@@ -133,25 +133,25 @@ class BUTTONS:
                 self.musicState = True
 
         else:
-            pygame.draw.rect(self.screen, blue, (self.width/2-400, 470, 200, 100))
+            pygame.draw.rect(self.screen, blue, (self.width / 2 - 400, 470, 200, 100))
         # quit
-        if self.width/2+400 > mouse[0] > self.width/2+200 and 570 > mouse[1] > 470:
-            pygame.draw.rect(self.screen, lightBlue, (self.width/2+200, 470, 200, 100))
-            if click[0] ==1:
+        if self.width / 2 + 400 > mouse[0] > self.width / 2 + 200 and 570 > mouse[1] > 470:
+            pygame.draw.rect(self.screen, lightBlue, (self.width / 2 + 200, 470, 200, 100))
+            if click[0] == 1:
                 pygame.display.quit()
                 pygame.quit()
                 exit()
         else:
-            pygame.draw.rect(self.screen, blue, (self.width/2+200, 470, 200, 100))
+            pygame.draw.rect(self.screen, blue, (self.width / 2 + 200, 470, 200, 100))
 
         startSurf, startRect = self.textObjects("Start",self.smallFont)
         musicSurf, musicRect = self.textObjects("Music",self.smallFont)
         quitSurf, quitRect = self.textObjects("Quit",self.smallFont)
 
 
-        startRect.center=(self.width/2-100+100, 470+50)
-        musicRect.center=(self.width/2-400+100, 470+50)
-        quitRect.center=(self.width/2+200+100, 470+50)
+        startRect.center=(self.width / 2 - 100 + 100, 470 + 50)
+        musicRect.center=(self.width / 2 - 400 + 100, 470 + 50)
+        quitRect.center=(self.width / 2 + 200 + 100, 470 + 50)
 
         self.screen.blit(startSurf, startRect)
         self.screen.blit(musicSurf, musicRect)
@@ -165,17 +165,17 @@ class BUTTONS:
         mouse = pygame.mouse.get_pos()
         click = pygame.mouse.get_pressed()
         # start
-        if self.width/2+100 > mouse[0] > self.width/2-100 and 470 > mouse[1] > 370:
-            pygame.draw.rect(self.screen, lightGreen, (self.width/2-100, 370, 200, 100))
-            if click[0]==1:
+        if self.width / 2 + 100 > mouse[0] > self.width / 2 - 100 and 470 > mouse[1] > 370:
+            pygame.draw.rect(self.screen, lightGreen, (self.width / 2 - 100, 370, 200, 100))
+            if click[0] == 1:
                 self.enterSwitch = True
         else:
-            pygame.draw.rect(self.screen, green, (self.width/2-100, 370, 200, 100))
+            pygame.draw.rect(self.screen, green, (self.width / 2 - 100, 370, 200, 100))
         if self.enterSwitch:
             self.enterYourName = username
 
         startSurf, startRect = self.textObjects(self.enterYourName,self.smallFont)
-        startRect.center=(self.width/2-100+100, 370+50)
+        startRect.center=(self.width / 2 - 100 + 100, 370 + 50)
         self.screen.blit(startSurf, startRect)
 
 
